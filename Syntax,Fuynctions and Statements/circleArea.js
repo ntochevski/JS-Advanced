b4496b70e1b47b3070acae9e58b1a447794ab732
ntochevski <tochevski.nikolay@gmail.com>
2021-12-26 15:33:14 +0200

    Problem Solving

--- Syntax,Fuynctions and Statements/circleArea.js
+++ Syntax,Fuynctions and Statements/circleArea.js
@@ -1,7 +1,8 @@
 function solve(input) {
     let result = '';
     typeof(input) === 'number' ? result = (Math.pow(input, 2) * Math.PI).toFixed(2) : 
-    result = `We can not calculate the circle area, because we receive a {type of argument}.`;
+    result = `We can not calculate the circle area, because we receive a ${typeof input}.`;
     console.log(result);
 }
 
+solve ('name')
---
+++ Syntax,Fuynctions and Statements/mathOperations.js
@@ -0,0 +1,12 @@
+function solve(a, b, operation) {
+
+    let obj = {
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b,
+        '*': (a, b) => a * b,
+        '/': (a, b) => a / b,
+        '%': (a, b) => a % b,
+        '**': (a, b) => a ** b,
+    }
+    console.log(obj[operation](a, b));
+}
