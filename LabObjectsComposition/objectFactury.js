31194ea3e101f0a24977000e76d9b9c43e759e35
Nikolay Tochevski <94860351+ntochevski@users.noreply.github.com>
2022-01-19 15:19:23 +0200

    commit

---
+++ LabObjectsComposition/objectFactury.js
@@ -0,0 +1,45 @@
+function factory(library, orders) {
+    let product = [];
+    for (const el of orders) {
+        let current = Object.assign({}, el.template);
+        let parts = el.parts;
+        for (const part of parts) {
+         current[part] = library[part];
+        }
+        product.push(current);
+    }
+    return product;
+}
+
+const library = {
+    print: function () {
+      console.log(`${this.name} is printing a page`);
+    },
+    scan: function () {
+      console.log(`${this.name} is scanning a document`);
+    },
+    play: function (artist, track) {
+      console.log(`${this.name} is playing '${track}' by ${artist}`);
+    },
+  };
+  const orders = [
+    {
+      template: { name: 'ACME Printer'},
+      parts: ['print']      
+    },
+    {
+      template: { name: 'Initech Scanner'},
+      parts: ['scan']      
+    },
+    {
+      template: { name: 'ComTron Copier'},
+      parts: ['scan', 'print']      
+    },
+    {
+      template: { name: 'BoomBox Stereo'},
+      parts: ['play']      
+    }
+  ];
+  const products = factory(library, orders);
+  console.log(products);
+  
