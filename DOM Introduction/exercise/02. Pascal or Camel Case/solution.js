26d5b65b4ed5e8f3c07eb1258ddfa65b88c21780
Nikolay Tochevski <94860351+ntochevski@users.noreply.github.com>
2022-01-22 21:03:39 +0200

    Commit

--- DOM Introduction/exercise/02. Pascal or Camel Case/solution.js
+++ DOM Introduction/exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,38 @@
 function solve() {
-  //TODO...
+  let text = document.getElementById('text').value;
+  let convention = document.getElementById('naming-convention').value;
+  text = text.toLowerCase().split('');
+  let result = [];
+  if (convention == 'Camel Case') {
+    let isUpperCase = false;
+    for (let letter of text) {
+      if (isUpperCase) {
+        letter = letter.toUpperCase();
+        result.push(letter)
+        isUpperCase = false;
+        continue;
+      }
+      if (letter !== ' ') {
+        result.push(letter);
+      } else {
+        isUpperCase = true;
+      }
+    }
+  } else if (convention == 'Pascal Case') {
+    let isBig = true;
+    for (let letter of text) {
+      if (isBig) {
+        letter = letter.toUpperCase();
+        isBig = false;
+      }
+      if (letter === ' ') {
+        isBig = true;
+        continue;
+      }
+      result.push(letter);
+    }
+  } else {
+    result = ['Error!'];
+  }
+  document.getElementById('result').textContent = result.join('');
 }
